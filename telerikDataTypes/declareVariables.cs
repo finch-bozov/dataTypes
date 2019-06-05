aea148fde6a2c70882d4f24e4aa27e81940bc847
Peter Bozov <51159178+finch-bozov@users.noreply.github.com>
2019-06-05 09:45:29 +0300

    Add files via upload

---
+++ telerikDataTypes/declareVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace telerikDataTypes
+{
+    class declareVariables
+    {
+        static void Main(string[] args)
+        {
+            short negativeValue = -115;
+            byte shortValue = 97;
+            uint shortInteger = 4825932;
+            int anotherInteger = -10000;
+            Console.WriteLine(negativeValue);
+            Console.WriteLine(shortValue);
+            Console.WriteLine(shortInteger);
+            Console.WriteLine(anotherInteger);
+        }
+    }
+}
